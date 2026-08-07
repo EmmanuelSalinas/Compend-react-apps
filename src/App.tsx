8abacacd63faf0b6fdeba335a844c0d4b1090e35
Emmanuel Salinas Celestino <mane@MacBook-Air-de-Emmanuel.local>
2026-08-06 22:33:46 -0600

    feat(Components):add custom components with props and state vars data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import Titulo from "./Titulo";
+import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
 
 function App() {
-  return <Titulo />;
+  const list = ["Goku", "Naruto", "Tanjiro"];
+
+  const handleSelect = (element: string) => {
+    console.log(element);
+  };
+  const handleSelect2 = (element: string) => {
+    console.log("Segundo handle: ", element);
+  };
+
+  /* return <CardBody title={"Hola mundo"} text={"El texto del componente."} />; */
+  return (
+    <Card>
+      <CardBody title={"Hola mundo"} text={"El texto del componente."} />
+      <List data={list} onSelect={handleSelect} />
+      <List data={list} onSelect={handleSelect2} />
+    </Card>
+  );
 }
 
 export default App;
